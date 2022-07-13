eb2bfa827aba023950a9bad8470b4b198f850ce8
mmd <abusemmd@gmail.com>
2022-07-14 00:03:50 +0430

    model changed

--- ariana/models.py
+++ ariana/models.py
@@ -15,23 +15,3 @@ class User(me.Document):
     tg_uid = me.IntField()
     active = me.BooleanField()
     make_new_private = me.BooleanField()
-    ariana_setting = me.DictField(default={
-
-        'Speaker': 'Male2',
-        'PitchLevel': 4,
-        'PunctuationLevel': 2,
-        'SpeechSpeedLevel': 5,
-        'ToneLevel': 10,
-        'GainLevel': 3,
-        'BeginningSilence': 0,
-        'EndingSilence': 0,
-
-    })
-    # ariana_setting:dict =                         speaker=r.Speaker,
-    #     #     #                     pitch_level=str(r.PitchLevel),
-    #     #     #                     punctuation_level=str(r.PunctuationLevel),
-    #     #     #                     speech_speed_level=str(r.SpeechSpeedLevel),
-    #     #     #                     tone_level=str(r.ToneLevel),
-    #     #     #                     gain_level=str(r.GainLevel),
-    #     #     #                     beginning_silence=str(r.BeginningSilence),
-    #     #     #                     ending_silence=str(r.EndingSilence),
